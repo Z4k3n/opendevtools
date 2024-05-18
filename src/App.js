f3c4de260747b23c8ce6d15912818cd8d0e19446
Guillermo <>
2024-05-18 22:13:33 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>From developers, to developers</h2>
+        <p>All kind of Open Source resources free to use! Get you website to the next level!</p>
+      </header>
+      <main>
+        <div class="grid-container">
+
+          <div>
+            <button className="shadow__btn">
+              web elements
+            </button>
+          </div>
+          <div>
+            <button className="shadow__btn">
+              Apis
+            </button>
+          </div>
+          <div>
+            <button className="shadow__btn">
+              IDE / editors
+            </button>
+          </div>
+          <div>
+            <button className="shadow__btn">
+              courses
+            </button>
+          </div>
+          <div>
+            <button className="shadow__btn">
+              data bases
+            </button>
+          </div>
+          <div>
+            <button className="shadow__btn">
+              design
+            </button>
+          </div>
+          <div>
+            <button className="shadow__btn">
+              Hosting
+            </button>
+          </div>
+          <div>
+            <button className="shadow__btn">
+              Forums
+            </button>
+          </div>
+
+
+
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
