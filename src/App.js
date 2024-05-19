f4745baf5c35363427be185c3eae8dbf385fd51a
Guillermo <>
2024-05-19 03:02:41 +0200

    2 version of main design

--- src/App.js
+++ src/App.js
@@ -2,57 +2,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>From developers, to developers</h2>
-        <p>All kind of Open Source resources free to use! Get you website to the next level!</p>
+    <div className="App fade-in">
+      <nav className="navbar">
+        <div className="navbar-brand">OpenDevTools</div>
+        <label class="switch">
+          <input type="checkbox" />
+          <span class="slider"></span>
+        </label>
+      </nav>
+
+      <header className="App-header fade-in" style={{ animationDelay: '0s' }}>
+        <h2>Empowering Developers Everywhere</h2>
+        <p>Discover a vast collection of free, open-source resources designed to elevate your projects. Start building better, faster, and smarter today!</p>
       </header>
       <main>
-        <div class="grid-container">
-
-          <div>
-            <button className="shadow__btn">
-              web elements
-            </button>
+        <div className="grid-container fade-in">
+          <div className="grid-item fade-in" style={{ animationDelay: '0.2s' }}>
+            <button className="learn-more">Web Elements</button>
           </div>
-          <div>
-            <button className="shadow__btn">
-              Apis
-            </button>
+          <div className="grid-item fade-in" style={{ animationDelay: '0.4s' }}>
+            <button className="learn-more">APIs</button>
           </div>
-          <div>
-            <button className="shadow__btn">
-              IDE / editors
-            </button>
+          <div className="grid-item fade-in" style={{ animationDelay: '0.6s' }}>
+            <button className="learn-more">IDEs & Editors</button>
           </div>
-          <div>
-            <button className="shadow__btn">
-              courses
-            </button>
+          <div className="grid-item fade-in" style={{ animationDelay: '0.8s' }}>
+            <button className="learn-more">Courses</button>
           </div>
-          <div>
-            <button className="shadow__btn">
-              data bases
-            </button>
+          <div className="grid-item fade-in" style={{ animationDelay: '1s' }}>
+            <button className="learn-more">Databases</button>
           </div>
-          <div>
-            <button className="shadow__btn">
-              design
-            </button>
+          <div className="grid-item fade-in" style={{ animationDelay: '1.2s' }}>
+            <button className="learn-more">Design Tools</button>
           </div>
-          <div>
-            <button className="shadow__btn">
-              Hosting
-            </button>
+          <div className="grid-item fade-in" style={{ animationDelay: '1.4s' }}>
+            <button className="learn-more">Hosting</button>
           </div>
-          <div>
-            <button className="shadow__btn">
-              Forums
-            </button>
+          <div className="grid-item fade-in" style={{ animationDelay: '1.6s' }}>
+            <button className="learn-more">Forums</button>
           </div>
-
-
-
         </div>
       </main>
     </div>
